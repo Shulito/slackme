011422289551586f10a0440109945ff295dad1aa
Julián Zorzenón <comm.ass@gmail.com>
2020-04-05 13:13:25 -0300

    Fix selection algorithm

--- slackme.js
+++ slackme.js
@@ -1,24 +1,22 @@
-let selectChannel = false;
-
 setInterval(
     () => {
-        if (selectChannel) {
+        const selectedItem = document.querySelectorAll('[data-qa-channel-sidebar-channel-is-selected="true"]');
+
+        if (selectedItem.length > 0) {
+            selectedItem[0].click();
+        } else {
             const channelList = document.querySelectorAll('[data-qa-channel-sidebar-channel-type="channel"]');
 
             if (channelList.length > 0) {
-                const channel = channelList[Math.floor(Math.random() * channelList.length)];
-                channel.click();
-            }
-        } else {
-            const usersConversationList = document.querySelectorAll('[data-qa-channel-sidebar-channel-type="im"]');
+                channelList[Math.floor(Math.random() * channelList.length)].click();
+            } else {
+                const usersConversationList = document.querySelectorAll('[data-qa-channel-sidebar-channel-type="im"]');
 
-            if (usersConversationList.length > 0) {
-                const usersConversation = usersConversationList[Math.floor(Math.random() * usersConversationList.length)];
-                usersConversation.click();
+                if (usersConversationList.length > 0) {
+                    usersConversationList[Math.floor(Math.random() * usersConversationList.length)].click();
+                }
             }
         }
-
-        selectChannel = !selectChannel;
     },
     60000
 );
