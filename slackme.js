3b2ce139f3463713b9e1f9a2e8abeeb6305d3559
Julián Zorzenón <comm.ass@gmail.com>
2020-04-08 11:28:16 -0300

    Fix unused variable

--- slackme.js
+++ slackme.js
@@ -7,14 +7,12 @@ setInterval(
 
             if (channelList.length > 0) {
                 channelList[Math.floor(Math.random() * channelList.length)].click();
-                successfulClick = true;
             }
         } else {
             const usersConversationList = document.querySelectorAll('[data-qa-channel-sidebar-channel-type="im"]');
 
             if (usersConversationList.length > 0) {
                 usersConversationList[Math.floor(Math.random() * usersConversationList.length)].click();
-                successfulClick = true;
             }
         }
 
